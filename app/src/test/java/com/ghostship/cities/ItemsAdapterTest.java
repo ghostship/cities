3a8e0879566751cc3b84ecac2b7345017ac7527b
Scott Keller <scott@inspiringapps.com>
2019-11-22 18:50:49 -0700

    Test

---
+++ app/src/test/java/com/ghostship/cities/ItemsAdapterTest.java
@@ -0,0 +1,28 @@
+package com.ghostship.cities;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ItemsAdapterTest {
+
+  @Test
+  public void onCreateViewHolder() {
+  }
+
+  @Test
+  public void onBindViewHolder() {
+  }
+
+  @Test
+  public void getItemCount() {
+  }
+
+  @Test
+  public void setItems() {
+  }
+
+  @Test
+  public void setOnItemClickListener() {
+  }
+}
